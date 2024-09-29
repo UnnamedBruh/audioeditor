47fa85e02d53457d4e01ebcb85782f7a453bfbc8
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2024-09-29 15:37:12 -0700

    Update editor.js

--- editor.js
+++ editor.js
@@ -1,150 +1,219 @@
-class FloatExporter {
-	constructor(audioData = new Float32Array([]), sampleRate = 48000) {
-		if (!(audioData instanceof Float32Array)) {
-			console.warn("The audioData must be a Float32Array. Otherwise, the audio data is automatically empty");
-			this.audioData = new Float32Array([]);
-		} else {
-			this.audioData = audioData;
+var FloatExporter = (function() {
+	function isNumber(msg, v) {
+		if (typeof v !== "number") {
+			throw new Error(msg + " must be a number, not a '" + (typeof sampleRate) + "'")
 		}
-		if (typeof sampleRate !== "number") {
-			console.warn("The sample rate must be a number, not a '" + (typeof sampleRate) + "'")
-			sampleRate = 48000;
-		} else {
-			if (sampleRate === 0) {
-				console.warn("The sample rate must not be 0, which this problem unsolved can cause infinitely long audio")
+	}
+	class FloatExporter {
+		constructor(audioData = new Float32Array([]), sampleRate = 48000) {
+			if (!(audioData instanceof Float32Array)) {
+				console.warn("The audioData must be a Float32Array. Otherwise, the audio data is automatically empty");
+				this.audioData = new Float32Array([]);
+			} else {
+				this.audioData = audioData;
+			}
+			if (typeof sampleRate !== "number") {
+				console.warn("The sample rate must be a number, not a '" + (typeof sampleRate) + "'")
 				sampleRate = 48000;
+			} else {
+				if (sampleRate === 0) {
+					console.warn("The sample rate must not be 0, which this problem unsolved can cause infinitely long audio")
+					sampleRate = 48000;
+				}
+				if (sampleRate < 0) console.warn("The sample rate cannot be ≤ 0, though this case is handled")
 			}
-			if (sampleRate < 0) console.warn("The sample rate cannot be ≤ 0, though this case is handled")
-		}
-		if (sampleRate % 1 !== 0) {
-			console.warn("The sample rate isn't rounded, but this case is handled")
-		}
-		this.sampleRate = Math.abs(Math.round(sampleRate));
-		this.backupData = new Float32Array(this.audioData);
-		this.FX = {
-			gain: multiplier => {
-				if (multiplier === 1) return false;
-				if (multiplier === 0) {
-					this.audioData.fill(0)
-					return false;
-				}
-				const de = this.audioData.length
-				for (let i = 0; i !== de; i++) {
-					this.audioData[i] *= multiplier
-				}
-				return true
-			},
-			speed: multiplier => {
-				if (multiplier === 1 || multiplier === -1) {
-					return false;
-				} else {
-					let len = this.audioData.length;
-					if (multiplier >= len) {
-						console.warn("The audio will be practically unhearable if its multiplier is bigger than the audio's buffer size. Returning an empty buffer now.");
-						this.audioData = new Float32Array([]);
+			if (sampleRate % 1 !== 0) {
+				console.warn("The sample rate isn't rounded, but this case is handled")
+			}
+			this.sampleRate = Math.abs(Math.round(sampleRate));
+			this.backupData = new Float32Array(this.audioData);
+			this.FX = {
+				gain: multiplier => {
+					isNumber("The volume multiplier", multiplier)
+					if (multiplier === 1) return false;
+					if (multiplier === 0) {
+						this.audioData.fill(0)
 						return false;
-					} else if (multiplier === 0) {
-						throw new Error("Prevented an infinite loop and a memory overflow, due to how the audio is processed.");
-					} else if (multiplier < 0) {
-						multiplier *= -1
-						console.warn("The module would crash and the audio will be reversed if the multiplier would be ≤ 0. Did you mean to use", multiplier, "for the multiplier instead?");
 					}
-					const changedArray = new Float32Array(Math.ceil(this.audioData.length * (1 / multiplier)));
-					len = changedArray.length
-					for (let i = 0; i !== len; i++) {
-						changedArray[i] = this.audioData[Math.floor(i * multiplier)];
+					const de = this.audioData.length
+					for (let i = 0; i !== de; i++) {
+						this.audioData[i] *= multiplier
 					}
-					this.audioData = changedArray;
-					return true;
-				}
-			},
-			distort: () => {
-				const de = this.audioData.length;
-				if (de === 0) return false;
-				if (de === 1) {
-					this.audioData[0] = Math.tanh(this.audioData[0])
-					return true;
-				}
-				for (let i = 0; i !== de; i++) {
-					this.audioData[i] = Math.tanh(this.audioData[i]);
-				}
-				return true;
-			},
-			quantize: bits => {
-				const de = this.audioData.length;
-				if (de === 0) return false;
-				const step = Math.pow(2, -bits);
-				if (de === 1) {
-					this.audioData[0] = Math.round(this.audioData[0] / step) * step;
-					return true;
-				}
-				for (let i = 0; i !== de; i++) {
-					this.audioData[i] = Math.round(this.audioData[i] / step) * step;
-				}
-				return true;
-			},
-			reduceFrequency: _ => {
-				console.warn("This effect can be computationally slower than other methods! You can use it anyway after this warning, though")
-				this.FX.reduceFrequency = sample => {
-					if (sample > 0 && sample <= 1) {
-						console.warn("The sample input must not be ≤ 1. Otherwise, the audio will not be affected very well")
-						return false
-					} else if (sample <= 0) {
-						throw new Error("The sample is ≤ 0, which can cause an infinite loop for the 'reduceFrequency' effect, and potentially cause a memory overflow (reversing the audio infinitely)")
-					}
-					const de = this.audioData.length;
-					if (de === 1 || de === 0) return false;
-					const s = Math.ceil(sample) - 1
-					let c, e;
-					for (let j = 0; j < de; j += sample) {
-						c = this.audioData[(e = Math.floor(j))];
-						for (let i = 0; i !== s && i + j < de; i++) {
-							this.audioData[i + e] = c;
+					return true
+				},
+				speed: multiplier => {
+					isNumber("The speed multiplier", multiplier)
+					if (multiplier === 1 || multiplier === -1) {
+						return false;
+					} else {
+						let len = this.audioData.length;
+						if (multiplier >= len) {
+							console.warn("The audio will be practically unhearable if its multiplier is bigger than the audio's buffer size. Returning an empty buffer now.");
+							this.audioData = new Float32Array([]);
+							return false;
+						} else if (multiplier === 0) {
+							throw new Error("Prevented an infinite loop and a memory overflow, due to how the audio is processed.");
+						} else if (multiplier < 0) {
+							multiplier *= -1
+							console.warn("The module would crash and the audio will be reversed if the multiplier would be ≤ 0. Did you mean to use", multiplier, "for the multiplier instead?");
 						}
+						const changedArray = new Float32Array(Math.ceil(this.audioData.length * (1 / multiplier)));
+						len = changedArray.length
+						for (let i = 0; i !== len; i++) {
+							changedArray[i] = this.audioData[Math.floor(i * multiplier)];
+						}
+						this.audioData = changedArray;
+						return true;
+					}
+				},
+				distort: () => {
+					const de = this.audioData.length;
+					if (de === 0) return false;
+					if (de === 1) {
+						this.audioData[0] = Math.tanh(this.audioData[0])
+						return true;
+					}
+					for (let i = 0; i !== de; i++) {
+						this.audioData[i] = Math.tanh(this.audioData[i]);
 					}
 					return true;
+				},
+				quantize: bits => {
+					isNumber("The bits limiter", bits)
+					const de = this.audioData.length;
+					if (de === 0) return false;
+					const step = Math.pow(2, -bits);
+					if (de === 1) {
+						this.audioData[0] = Math.round(this.audioData[0] / step) * step;
+						return true;
+					}
+					for (let i = 0; i !== de; i++) {
+						this.audioData[i] = Math.round(this.audioData[i] / step) * step;
+					}
+					return true;
+				},
+				reduceFrequency: _ => {
+					console.warn("This effect can be computationally slower than other methods! You can use it anyway after this warning, though")
+					this.FX.reduceFrequency = sample => {
+						if (sample == undefined) {
+							return
+						}
+						isNumber("The samples factor", sample)
+						if (sample > 0 && sample <= 1) {
+							console.warn("The sample input must not be ≤ 1. Otherwise, the audio will not be affected very well")
+							return false
+						} else if (sample <= 0) {
+							throw new Error("The sample is ≤ 0, which can cause an infinite loop for the 'reduceFrequency' effect, and potentially cause a memory overflow (reversing the audio infinitely)")
+						}
+						const de = this.audioData.length;
+						if (de === 1 || de === 0) return false;
+						const s = Math.ceil(sample) - 1
+						let c, e;
+						for (let j = 0; j < de; j += sample) {
+							c = this.audioData[(e = Math.floor(j))];
+							for (let i = 0; i !== s && i + j < de; i++) {
+								this.audioData[i + e] = c;
+							}
+						}
+						return true;
+					}
+					return "effect can be used after warning";
+				},
+				doubleSpeed: () => {
+					isNumber("The speed multiplier", multiplier)
+					if (multiplier === 1 || multiplier === -1) {
+						return false;
+					} else {
+						let len = this.audioData.length;
+						if (multiplier >= len) {
+							console.warn("The audio will be practically unhearable if its multiplier is bigger than the audio's buffer size. Returning an empty buffer now.");
+							this.audioData = new Float32Array([]);
+							return false;
+						} else if (multiplier === 0) {
+							throw new Error("Prevented an infinite loop and a memory overflow, due to how the audio is processed.");
+						} else if (multiplier < 0) {
+							multiplier *= -1
+							console.warn("The module would crash and the audio will be reversed if the multiplier would be ≤ 0. Did you mean to use", multiplier, "for the multiplier instead?");
+						}
+						const changedArray = new Float32Array(Math.ceil(this.audioData.length * (1 / multiplier)));
+						len = changedArray.length
+						for (let i = 0; i !== len; i++) {
+							changedArray[i] = this.audioData[Math.floor(i * multiplier)];
+						}
+						this.audioData = changedArray;
+						return true;
+					}
+				},
+			}
+		}
+		convertToWav(exp = "blob") {
+			const numChannels = 1, ch1 = 32767, ch2 = 32758, ch3 = 0, ch4 = -1, ch5 = 1, len = this.audioData.length;
+			const len2 = len * 2;
+			const buffer = new ArrayBuffer(44 + len2);
+			const view = new DataView(buffer);
+			this.writeString(view, 0, 'RIFF');
+			view.setUint32(4, 36 + len2, true);
+			this.writeString(view, 8, 'WAVE');
+			this.writeString(view, 12, 'fmt ');
+			view.setUint32(16, 16, true);
+			view.setUint16(20, 1, true);
+			view.setUint16(22, numChannels, true);
+			view.setUint32(24, this.sampleRate, true);
+			view.setUint32(28, this.sampleRate * 2, true);
+			view.setUint16(32, 2, true);
+			view.setUint16(34, 16, true);
+			this.writeString(view, 36, 'data');
+			view.setUint32(40, len2, true);
+			let offset = 44, s;
+			for (let i = 0; i !== len; i++) {
+				s = Math.max(ch4, Math.min(ch5, this.audioData[i]));
+				view.setInt16(offset, s < ch3 ? s * ch2 : s * ch1, true);
+				offset += 2;
+			}
+			return exp === "blob" ? new Blob([view], { type: 'audio/wav' }) : exp === "dataview" ? view : undefined;
+		}
+		writeString(view, offset, string) {
+			for (let i = 0; i < string.length; i++) {
+				view.setUint8(offset + i, string.charCodeAt(i));
+			}
+		}
+		restore() {
+			this.audioData = this.backupData;
+			this.backupData = this.backupData.slice();
+		}
+		static sineWave(frequency, duration, sampleRate) {
+			const array = new Float32Array(Math.floor(duration * sampleRate))
+			const len = array.length, cache = 2 * Math.PI
+			for (let i = 0; i !== len; i++) {
+				array[i] = Math.sin((cache * frequency * i) / sampleRate)
+			}
+			return new FloatExporter(array, sampleRate)
+		}
+		static help(indicators) {
+			if (indicators) {
+				switch (indicators) {
+					case "fx gain":
+						console.log("{FloatExporter Class}.FX.gain multiplies the audio's volume by a single parameter:\n- multiplier: The multiplier that is used to multiply the audio's volume. For example, 0.5 halfens the audio's volume, while 2 doubles the audio's volume.\nWARNING: Keep in mind that very high values may result in distorted and extremely loud sound!")
+						break
+					case "fx gain":
+						console.log("{FloatExporter Class}.FX.speed divides the audio's sound duration, and multiplies the audio's pitch by a single parameter:\n- multiplier: The multiplier that is used to speed up or slow down the audio. For example, 0.5 slows down the audio, while 2 speeds up the audio.\nNOTE: Applying multiple of this effect would result in slightly choppy/pixely audio!\nWARNING: Keep in mind that very low values result in very long sound!")
+						break
+					case "fx distort":
+						console.log("{FloatExporter Class}.FX.distort distorts the audio without making it very loud, and it doesn't have any parameters.\nWARNING: This effect is irreversable, unless you use the {FloatExporter Class}.restore function to restore the original sound!")
+						break
+					case "fx reduceFrequency":
+						console.log("{FloatExporter Class}.FX.reduceFrequency uses a single parameter to lower the audio frequecies:\n- sample: The sample factor that would determine how low the frequencies should be. For example, 6 results in slightly low frequencies, while 32 results in incomprehensible audio.\nNOTE: You may get a warning from using this effect only ONCE.\nWARNING: This effect is irreversible, and is also resource-intensive! Make sure to apply this to shorter sounds!")
+						break
+					case "fx quantize":
+						console.log("{FloatExporter Class}.FX.quantize uses a single parameter to limit the data to (bits ** 4) possible values:\n- bits: The value to limit the possible values in. For example, 16 results in slightly staticy audio, while 4 results in very staticy audio.\nWARNING: This effect is irreversible, and can limit the ability to understand the audio!")
+						break
+					default:
+						console.warn("This directory doesn't exist yet. Perhaps it may get added soon.")
 				}
-				return "effect can be used after warning";
+			} else {
+				console.log("This function is supposed to help you understand how the module works, and how the effects are applied to the module. Here are all of the possible values that can be achieved:\n- 'fx gain'\n- 'fx speed'\n- 'fx distort'\n- fx quantize\n- 'fx reduceFrequency'\n- fx 'doubleSpeed'")
 			}
 		}
 	}
-	convertToWav(exp = "blob") {
-		const numChannels = 1, ch1 = 32767, ch2 = 32758, ch3 = 0, ch4 = -1, ch5 = 1, len = this.audioData.length;
-		const len2 = len * 2;
-		const buffer = new ArrayBuffer(44 + len2);
-		const view = new DataView(buffer);
-		this.writeString(view, 0, 'RIFF');
-		view.setUint32(4, 36 + len2, true);
-		this.writeString(view, 8, 'WAVE');
-		this.writeString(view, 12, 'fmt ');
-		view.setUint32(16, 16, true);
-		view.setUint16(20, 1, true);
-		view.setUint16(22, numChannels, true);
-		view.setUint32(24, this.sampleRate, true);
-		view.setUint32(28, this.sampleRate * 2, true);
-		view.setUint16(32, 2, true);
-		view.setUint16(34, 16, true);
-		this.writeString(view, 36, 'data');
-		view.setUint32(40, len2, true);
-		let offset = 44, s;
-		for (let i = 0; i !== len; i++) {
-			s = Math.max(ch4, Math.min(ch5, this.audioData[i]));
-			view.setInt16(offset, s < ch3 ? s * ch2 : s * ch1, true);
-			offset += 2;
-		}
-		return exp === "blob" ? new Blob([view], { type: 'audio/wav' }) : exp === "dataview" ? view : undefined;
-	}
-	writeString(view, offset, string) {
-		for (let i = 0; i < string.length; i++) {
-			view.setUint8(offset + i, string.charCodeAt(i));
-		}
-	}
-	static sineWave(frequency, duration, sampleRate) {
-		const array = new Float32Array(Math.floor(duration * sampleRate))
-		const len = array.length, cache = 2 * Math.PI
-		for (let i = 0; i !== len; i++) {
-			array[i] = Math.sin((cache * frequency * i) / sampleRate)
-		}
-		return new FloatExporter(array, sampleRate)
-	}
-}
+	return FloatExporter
+})()
